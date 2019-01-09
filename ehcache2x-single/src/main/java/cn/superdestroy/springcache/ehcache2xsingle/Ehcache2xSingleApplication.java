024cbca637c58071f20acdf9753e84571c85fae3
zhangningwei <46161524@qq.com>
2019-01-09 19:26:36 +0800

    配置完成Ehcache的使用。需要注意的是在spring boot内对于Ehcache有默认的配置。

--- ehcache2x-single/src/main/java/cn/superdestroy/springcache/ehcache2xsingle/Ehcache2xSingleApplication.java
+++ ehcache2x-single/src/main/java/cn/superdestroy/springcache/ehcache2xsingle/Ehcache2xSingleApplication.java
@@ -2,8 +2,11 @@ package cn.superdestroy.springcache.ehcache2xsingle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+//启用SpringCache的必要注解
+@EnableCaching
 public class Ehcache2xSingleApplication {
 
     public static void main(String[] args) {
--- ehcache2x-single/src/test/java/cn/superdestroy/springcache/ehcache2xsingle/Ehcache2xSingleApplicationTests.java
+++ ehcache2x-single/src/test/java/cn/superdestroy/springcache/ehcache2xsingle/Ehcache2xSingleApplicationTests.java
@@ -1,7 +1,9 @@
 package cn.superdestroy.springcache.ehcache2xsingle;
 
+import cn.superdestroy.springcache.ehcache2xsingle.cache.UserCache;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Ehcache2xSingleApplicationTests {
 
+    @Autowired
+    private UserCache userCache;
+
     @Test
     public void contextLoads() {
+        userCache.put("1");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(userCache.get("1"));
     }
 
 }
