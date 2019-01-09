c02449e14f49760336f0c0e28d95112caac24e42
zhangningwei <46161524@qq.com>
2019-01-09 17:11:22 +0800

    Initial commit

---
+++ ehcache2x-single/src/main/java/cn/superdestroy/springcache/ehcache2xsingle/Ehcache2xSingleApplication.java
@@ -0,0 +1,14 @@
+package cn.superdestroy.springcache.ehcache2xsingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ehcache2xSingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ehcache2xSingleApplication.class, args);
+    }
+
+}
+
